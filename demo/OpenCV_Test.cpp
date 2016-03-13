11db84d9779ed59dbd617ba770a526e4d35e144e
fengbc <fengbingchun@sensetime.com>
2016-03-13 11:22:12 +0800

    update to opencv3.1

--- demo/OpenCV_Test.cpp
+++ demo/OpenCV_Test.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 #include <opencv2/opencv.hpp>
+
 
 int main()
 {
 	cv::Mat matSrc = cv::imread("E:/GitCode/OpenCV_Test/test_images/lena.png");
+
+	cv::namedWindow("show image");
+	cv::imshow("show image", matSrc);
+	cv::waitKey(0);
+	cv::destroyWindow("show image");
 
 	std::cout << "ok" << std::endl;
 	return 0;
